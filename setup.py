918f59f1ccd5229e5d8efff70939d8fd2f1be73f
Drew Ipson <drewipson@gmail.com>
2021-09-12 23:31:36 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Cluster twitter users using common words and emojis found in their bios from the 2021 Superbowl.',
+    author='Drew Ipson',
+    license='MIT',
+)
